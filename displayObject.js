db65cb45d1586fa1b09ba1fb5a1f471818a9adeb
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2020-06-26 00:52:03 +0200

    Create displayObject.js

---
+++ displayObject.js
@@ -0,0 +1,13 @@
+
+		function book(title,author,page,){
+			this.title = title;
+			this.author = author;
+			this.page = page;
+
+			this.display = function(){
+				console.log(this);
+			}
+		}
+
+		var book1 = new book("Kahramanlar","yakup",10);
+		book1.display();
